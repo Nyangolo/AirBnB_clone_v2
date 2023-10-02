7b4c1146646e39148bb8419e8c7d373565df522f
Nyangolo <nyangoloc@gmail.com>
2023-10-02 10:10:42 -0700

    on the brink

--- models/review.py
+++ models/review.py
@@ -1,14 +1,18 @@
-#!/usr/bin/python
-""" holds class Review"""
-from models.base_model import BaseModel
+#!/usr/bin/python3
+"""This is the review class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Review(BaseModel):
-    """Representation of Review """
-    place_id = ""
-    user_id = ""
-    text = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
