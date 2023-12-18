3a21b437d65f4e128c0b80725a85e8f5869e1ef4
Nyangolo <nyangoloc@gmail.com>
2023-12-18 19:15:59 +0000

    onepage

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,17 +3,11 @@
 '''
 from flask import Flask
 
-
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
-
-@app.route('/')
-def index():
-    '''The home page.'''
-    return 'Hello HBNB!'
-
+@app.route('/airbnb-onepage/')
+def hello():
+    return 'Hello, this is the content served from /airbnb-onepage/'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
