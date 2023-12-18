5df2051846af6c050df5f1c47c25e7b134489520
Nyangolo <nyangoloc@gmail.com>
2023-12-18 19:50:28 +0000

    crap

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/airbnb-onepage/')
 def hello():
-    return 'Hello, this is the content served from /airbnb-onepage/'
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
